d606beefe097c9a290a77150f569384bf41b33c0
Rodrigo Silva <rodrigo.santos.silva@outlook.com>
2025-06-29 17:15:39 -0300

    configuração de requisições sem Spring Security

--- server/src/main/java/br/edu/utfpr/pb/project/server/config/WebConfig.java
+++ server/src/main/java/br/edu/utfpr/pb/project/server/config/WebConfig.java
@@ -3,6 +3,8 @@ package br.edu.utfpr.pb.project.server.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
@@ -10,5 +12,21 @@ public class WebConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    //método para permitir a conexão entre frontend e backend a partir de portas diferentes
+    //esta conexão válida para casos onde o Spring Security não é usado
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
